ed6a1a41bb1d4127340f39a20145b49f9c6596bb
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-04-15 17:01:57 +0200

    added-TransformFinalBlock-method

--- 3DES/3DES.cs
+++ 3DES/3DES.cs
@@ -6,14 +6,35 @@ namespace _3DES
 {
     public class TripleDes
     {
+        private const string securityKey = "DataSecurity3DES";
         public static string Encrypt(string TextToEncrypt)
         {
+            string encryptedText = "";
+            byte[] MyEncryptedArray = UTF8Encoding.UTF8.GetBytes(TextToEncrypt);
+            MD5CryptoServiceProvider MyMD5CryptoService = new MD5CryptoServiceProvider();
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash
+                (UTF8Encoding.UTF8.GetBytes(securityKey));
+            MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new
+                TripleDESCryptoServiceProvider();
+
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+
+            MyTripleDESCryptoService.Mode = CipherMode.ECB;
+
+            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var MyCryptoTransform = MyTripleDESCryptoService
+                .CreateEncryptor();
+
+            byte[] MyresultArray = MyCryptoTransform
+                .TransformFinalBlock(MyEncryptedArray, 0,
+                MyEncryptedArray.Length); 
 
         }
 
-        public static string Decrypt(string TextToDecrypt)
-        {
 
-        }
     }
 }
