287f759f9832ff864f04403f15ec8124eba92121
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-04-15 16:28:17 +0200

    Implement-TripleDESCryptoServiceProvider

--- 3DES/3DES.cs
+++ 3DES/3DES.cs
@@ -1,3 +1,19 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
+
+namespace _3DES
+{
+    public class TripleDes
+    {
+        public static string Encrypt(string TextToEncrypt)
+        {
+
+        }
+
+        public static string Decrypt(string TextToDecrypt)
+        {
+
+        }
+    }
+}
