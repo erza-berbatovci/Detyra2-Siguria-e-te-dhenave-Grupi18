da7086944ee125b3eab2a5207f3877d012b65109
Esra <esraqerimi12@gmail.com>
2023-04-15 22:10:43 +0200

    Update Program.cs

--- 3DES/Program.cs
+++ 3DES/Program.cs
@@ -1,1 +1,14 @@
-﻿jsanxscscsc
+﻿using System;
+using System.IO;
+using System.Reflection;
+using _3DES;
+using System.Security.Cryptography;
+
+namespace TripleDes {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Do you want to encrypt text or file? ");
+            string input = Console.ReadLine();
+        }
+    }
+}
